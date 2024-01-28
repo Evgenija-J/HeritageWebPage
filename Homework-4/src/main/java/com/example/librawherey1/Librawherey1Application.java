47d3939edd71940d0b241d0fad960d0e773fd43d
Evgenija Jankulovska <e.jankulovska0207@gmail.com>
2024-01-28 19:01:03 +0100

    refactored application classes

---
+++ Homework-4/src/main/java/com/example/librawherey1/Librawherey1Application.java
@@ -0,0 +1,13 @@
+package com.example.librawherey1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Librawherey1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Librawherey1Application.class, args);
+    }
+
+}
---
+++ Homework-4/src/test/java/com/example/librawherey1/Librawherey1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librawherey1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Librawherey1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
